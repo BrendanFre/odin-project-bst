8db2bf21386da851fc28a0a287592868be2e149e
Brendan Freeman <bfreeman0@gmail.com>
2024-07-09 21:28:44 +0100

    feat(TREE.SPEC.KS): Added tests for children of root node

--- spec/Tree.spec.js
+++ spec/Tree.spec.js
@@ -1,10 +1,13 @@
 import Tree from "../src/Tree.js";
 
 describe("Tree", () => {
-  it("Should have the correct class name", () => {
-    const tree = new Tree([2, 5, 5]);
+  const tree = new Tree([2, 5, 5]);
+  it("Root node should have value of 5", () => {
     expect(tree.constructor.name).toEqual("Tree");
     expect(tree.root.value).toEqual(5);
-    console.log(tree);
+  });
+  it("Left child node should have value of 2", () => {
+    expect(tree.constructor.name).toEqual("Tree");
+    expect(tree.root.l.value).toEqual(2);
   });
 });
